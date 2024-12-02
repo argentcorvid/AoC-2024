aa5c40eb686bb4ad4a88386228da41ec0a022a85
ArgentCorvid <khokanson@gmail.com>
2024-12-01 23:01:10 -0600

    edit template. day 1 input parsing work

--- day1.lisp
+++ day1.lisp
@@ -1,14 +1,26 @@
-;;;dayx
+;;;day 1
 
-(defparameter +day-number+ x)
+(defparameter +day-number+ 1)
 (defparameter +working-dir+ (uiop:truenamize "~/aoc-2024/"))
 (defparameter +input-name-template+ "2024d~dinput.txt")
 
 (defparameter +test-input+
-  '())
+  (str:split #\newline
+             "3   4
+4   3
+2   5
+1   3
+3   9
+3   3"))
 
 (defun parse-input (lines)
-  )
+  (let ((str:*omit-nulls* t))
+    (loop for l in lines
+          for (lnum rnum) in (mapcar #'parse-integer (str:split #\space l))
+          collect lnum into left
+          collect rnum into right
+          finally (return (list (sort left  #'<)
+                                (sort right #'<))))))
 
 (defun p1 ()
   ) 
