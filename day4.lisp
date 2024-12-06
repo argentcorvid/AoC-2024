446bc8af8ccbfc34195621c8a8f80a4429681979
Hokanson <Kyle.Hokanson@emerson.com>
2024-12-06 14:23:43 -0600

    d4 test input filled in

---
+++ day4.lisp
@@ -0,0 +1,44 @@
+;;;day 4
+
+(defparameter +day-number+ 4)
+(defparameter +input-name-template+ "2024d~dinput.txt")
+
+(defparameter +test-input-1+
+"..X...
+.SAMX.
+.A..A.
+XMAS.S
+.X....")
+
+(defparameter +test-input-2+
+"MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX") ;; XMAS 18 times f,b,d
+
+(defun parse-input (lines)
+  )
+
+(defun p1 ()
+  ) 
+
+(defun p2 ()
+  )
+
+(defun main ()
+  (let* ((infile-name (format nil +input-name-template+ +day-number+))
+         (input-lines (uiop:read-file-lines infile-name))
+         (data (parse-input input-lines)))
+    (fresh-line)
+    (princ "part 1: ")
+    (princ (p1 data))
+    ;; (fresh-line)
+    ;; (princ "part 2: ")
+    ;; (princ (p2 data))
+    ))
