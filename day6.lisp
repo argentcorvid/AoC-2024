d8764773e61c296650a2636cb1488a4ae3160551
Hokanson <Kyle.Hokanson@emerson.com>
2024-12-16 09:54:48 -0600

    d6p1 find-next-obstacle done, start on move-guard

--- day6.lisp
+++ day6.lisp
@@ -26,9 +26,6 @@
 (defvar *directions* (pairlis '(n      e     s     w)
                               '((-1 0) (0 1) (1 0) (0 -1))))
 
-(defun move-guard ()
-  )
-
 (defun parse-input (input-string)
   (let ((obstacle-list (list))
         guard-start)
@@ -43,7 +40,7 @@
             do (setf guard-start (list line col))
           when (char= cell #\newline)
             do (when (= 0 line)
-                 (setf grid-width col))
+                 (setf grid-width (1- col)))
                (setf col -1)
                (incf line)
                (incf grid-length)
@@ -54,24 +51,25 @@
                                         :obstacles (nconc (fence min-max)
                                                           (nreverse obstacle-list))))))))
 
-(defun fence (min-max-list) ;making it 1 square bigger will make the total 1 more than needed
-  (let ((min-row (first (first min-max-list)))
-        (min-col (second (first min-max-list)))
-        (max-row (first (second min-max-list)))
-        (max-col (second (second min-max-list))))
+(defun fence (min-max-list &optional (extra 0)) ;making it 1 square bigger will make the total 1 more than needed
+  ;(declare (optimize (debug 3)))
+  (let ((min-row (- (first (first min-max-list)) extra))
+        (min-col (- (second (first min-max-list)) extra))
+        (max-row (+ (first (second min-max-list)) extra))
+        (max-col (+ (second (second min-max-list)) extra)))
     (remove-duplicates
      (nconc (mapcan (lambda (s)
                       (list (list min-row  s) (list s min-col)))
-                    (a:iota (+ 2 max-row)
-                      :start min-row))
+                    (a:iota (+ 2 max-row extra)
+                            :start min-row))
             (mapcan (lambda (s)
                       (list (list max-col s) (list s max-row)))
-                    (a:iota (+ 2 max-col)
+                    (a:iota (+ 2 max-col extra)
                             :start min-col)))
      :test 'equal)))
 
-(defun oob? (guard) ;rewrite, no longer actually goes out
-  (let* ((pos (guard-pos guard))
+(defun oob? (guard) ;rewrite, no longer actually goes out if extra = 0
+  (let* ((pos (guard-position guard))
          (bounds (guard-bounds guard))
          (row (first pos))
          (col (second pos)))
@@ -80,15 +78,22 @@
         (= col (cadar bounds))
         (= col (cadadr bounds)))))
 
+(defun man-dist (pt1 pt2)
+  "find the 'manhattan' distance between 2 points"
+    ;; (reduce #'+ (mapcar (lambda (a b)
+    ;;                       (abs (- a b)))
+    ;;              pt1 pt2)) ; conses a lot
+  (+ (abs (- (first pt1) (first pt2))) (abs (- (second pt1) (second pt2)))))
+
 (defun find-next-obstacle (guard)
   (let* ((elf-pos (guard-position guard))
          (elf-dir (cdr (assoc (guard-direction guard) *directions*)))
          (candidates (remove-if-not (lambda (obs-pos)
-                                      (and (or (= (first elf-pos)
+                                      (and (or (= (first elf-pos)  ;in the same row or column
                                                   (first obs-pos))
                                                (= (second elf-pos)
                                                   (second obs-pos)))
-                                           (equal (mapcar (lambda (o e)
+                                           (equal (mapcar (lambda (o e) ;in the same direction guard is pointing
                                                             (signum (- o e)))
                                                           obs-pos elf-pos)
                                                   elf-dir)))
@@ -97,11 +102,14 @@
           with min-c = (mapcar #'* ;something that is far away max row,col in bounds times dir?
                                (second (guard-bounds guard))
                                elf-dir) 
-          when (< (abs-dist elf-pos c)
-                  (abs-dist elf-pos min-c))
+          when (< (man-dist elf-pos c)
+                  (man-dist elf-pos min-c))
             do (setf min-c c)
           end
           finally (return min-c))))
+
+(defun move-guard (next-obstacle)
+  )
 
 (defun p1 (guard)
   (loop
@@ -117,8 +125,8 @@
     (setf parts-list (list parts-list)))
   (dolist (part parts-list)
     (ccase part
-      (1 (format t "~&Part 1: ~a" (p1 (copy-structure guard))))
-      (2 (format t "~&Part 2: ~a" (p2 (copy-structure guard)))))))
+      (1 (format t "~&Part 1: ~a" (p1 (copy-guard guard))))
+      (2 (format t "~&Part 2: ~a" (p2 (copy-guard guard)))))))
 
 (defun main (parts-list)
   (let* ((infile-name (format nil +input-name-template+ +day-number+))
