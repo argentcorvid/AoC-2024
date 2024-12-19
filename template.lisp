a75296d6e9248723d9faa3a4055639fd0432e5fa
Hokanson <Kyle.Hokanson@emerson.com>
2024-12-19 16:03:35 -0600

    template: compile and load-in alexandria and str, set a as PLN.

--- template.lisp
+++ template.lisp
@@ -1,9 +1,14 @@
 ;;;dayx
 
-(defparameter +day-number+ x)
-(defparameter +input-name-template+ "2024d~dinput.txt")
+(eval-when (:compile-toplevel :load-toplevel)
+  (ql:quickload '(:alexandria :str))
+  #+SBCL (add-package-local-nickname 'a 'alexandria)
+  #+ECL  (ext:add-package-local-nickname 'a 'alexandria))
 
-(defparameter +test-input+
+(defparameter *day-number* x)
+(defparameter *input-name-template* "2024d~dinput.txt")
+
+(defparameter *test-input*
   '())
 
 (defun parse-input (lines)
@@ -16,18 +21,16 @@
   )
 
 (defun run (parts-list data)
-  (unless (consp parts-list)
-    (setf parts-list (list parts-list)))
-  (dolist (part parts-list)
+  (dolist (part (a:ensure-list parts-list))
     (ccase part
       (1 (format t "~&Part 1: ~a" (p1 data)))
       (2 (format t "~&Part 2: ~a" (p2 data))))))
 
-(defun main (parts-list)
-  (let* ((infile-name (format nil +input-name-template+ +day-number+))
+(defun main (&rest parts)
+  (let* ((infile-name (format nil *input-name-template* *day-number*))
          (input-lines (uiop:read-file-lines infile-name))
          (data (parse-input input-lines)))
-    (run parts-list data)))
+    (run parts data)))
 
-(defun test (parts-list)
-  (run parts-list (parse-input +test-data+)))
+(defun test (parts)
+  (run parts (parse-input *test-data*)))
