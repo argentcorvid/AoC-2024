34b2117d091253d31fd7f97e099e0b3c4cc93ded
ArgentCorvid <khokanson@gmail.com>
2024-12-23 17:13:12 -0600

    d8p1 gets 11 instead of 14 for test input

--- day8.lisp
+++ day8.lisp
@@ -9,7 +9,7 @@
 (defparameter *input-name-template* "2024d~dinput.txt")
 
 (defparameter *test-input*
-  "............
+ (str:split #\newline "............
 ........0...
 .....0......
 .......0....
@@ -21,7 +21,8 @@
 .........A..
 ............
 ............
-")
+"
+            :omit-nulls t))
 
 (defun parse-input (lines)
   (let ((grid (make-hash-table))
@@ -30,19 +31,39 @@
           for line in lines
           do (ppcre:do-matches (c end antenna-scanner line)
                (let ((antenna-value (char line c)))
-                 (setf (gethash (complex l c) grid) antenna-value))))
+                 (push (complex l c) (gethash antenna-value grid)))))
+    (setf (gethash 'rows grid) (length lines))
+    (setf (gethash 'cols grid) (length (first lines)))
     grid))
 
-(defun p1 ()
-  ) 
+(defun p1 (grid)
+  (let ((antinodes (make-hash-table))
+        (maxrow (1+ (gethash 'rows grid)))
+        (maxcol (1+ (gethash 'cols grid))))
+    (maphash (lambda (freq locs)
+               (when (and (characterp freq) ;becuase of rows and cols
+                          (< 1 (length locs)))
+                 (a:map-combinations (lambda (points)
+                                       (destructuring-bind (pt1 pt2) points
+                                         (let* ((dist (- pt2 pt1))
+                                                (an1 (+ pt1 (* 2 dist)))
+                                                (an2 (+ pt2 (* -2 dist))))
+                                           (dolist (an (list an1 an2))
+                                             (when (and (< 0 (realpart an) maxrow)
+                                                        (< 0 (imagpart an) maxcol))
+                                               (setf (gethash an antinodes) t))))))
+                                     locs :length 2)))
+             grid)
+    antinodes)) 
 
 (defun p2 ()
   )
 
-(defun run (Zparts-list data)
+(defun run (parts-list data)
   (dolist (part (a:ensure-list parts-list))
     (ccase part
-      (1 (format t "~&Part 1: ~a" (p1 data)))
+      (1 (let ((p1-answer (hash-table-count (p1 data))))
+           (format t "~&Part 1: ~a" p1-answer)))
       (2 (format t "~&Part 2: ~a" (p2 data))))))
 
 (defun main (&rest parts)
@@ -52,4 +73,4 @@
     (run parts data)))
 
 (defun test (parts)
-  (run parts (parse-input *test-data*)))
+  (run parts (parse-input *test-input*)))
