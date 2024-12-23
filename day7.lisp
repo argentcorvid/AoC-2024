fdab5a276e679d4a25588201db86e3320d1ee99e
ArgentCorvid <khokanson@gmail.com>
2024-12-22 21:11:48 -0600

    d7p2 works! used string ops instead of math.

--- day7.lisp
+++ day7.lisp
@@ -22,17 +22,42 @@
 "
              :omit-nulls t))
 
-;(defun parse-input (lines))
+(defparameter *ti2*
+  (str:split #\newline "13: 6 7 3
+42: 6 7 3
+10: 6 7 3
+21: 6 7 3
+"
+             :omit-nulls t))
+
+(defparameter *ti3*
+  (str:split #\newline "40656: 26 37 33 56 79 42 60 33
+20592: 46 62 81 97 72
+39022201: 23 47 69 82 69 83 41
+536008016: 73 91 24 78 75 93 11 36 42 86 25 24 66 26
+81422384: 57 64 82 84 84 43 17 16
+5575106866126011: 94 43 54 52 36 29 26 94 55 84 27 87 97
+5172: 66 71 36 25 74 50 91
+206506492: 21 31 19 67 87 16 86 69 38 12 13 10
+1030269780930: 75 50 16 78 66 41 39 54 11 83 54 34 87
+2731294098: 91 27 19 32 27 15 94 18
+"
+             :omit-nulls t))
+
+                                        ;(defun parse-input (lines))
 
 (defun possible (result number-list &optional (ops (list #'/ #'-)))
   (labels ((%possible (prev nl)
+             (when (or (not (plusp prev))
+                       (not (integerp prev)))
+               (return-from %possible nil))
              (destructuring-bind (a . rest) nl
                (when (endp rest)
                  (return-from %possible (= prev a)))
                (loop for op in ops
                      when (%possible (funcall op prev a)
-                                         rest)
-                         return t))))
+                                     rest)
+                       return t))))
     (%possible result (nreverse number-list))))
 
 (defun endswith (num1 num2)
@@ -41,12 +66,21 @@
       (zerop (expt (mod (- num1 num2) 10)
                    (digits num2)))))
 (defun digits (n)
-  (ceiling (log n 10)))
+  (1+ (floor (log n 10))))
 
 (defun decat (num1 num2)
   (if (endswith num1 num2)
       (floor num1 (expt 10 (digits num2)))
-      0))
+      -1))
+
+(defun decat-dumb (num1 num2)
+  (let* ((n1 (format nil "~d" num1))
+         (n2 (format nil "~d" num2))
+         (st (- (length n1) (length n2))))
+    (if (and (plusp st)
+             (string= n1 n2 :start1 st))
+        (parse-integer n1 :end st)
+        -1)))
 
 (defun p1 (lines)
   (loop for line in lines
@@ -59,7 +93,7 @@
   (loop for line in lines
         for (result . numbers) = (mapcar (a:rcurry #'parse-integer :junk-allowed t)
                                          (str:split #\space line :omit-nulls t))
-        when (possible result numbers (list #'/ #'decat #'-))
+        when (possible result numbers (list #'/ #'decat-dumb #'-))
           sum result))
 
 (defun run (parts-list data)
