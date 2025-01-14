60cebc1dfb2517d44ddfd256ebb40e0cb362ee49
Hokanson <Kyle.Hokanson@emerson.com>
2025-01-14 17:04:52 -0600

    d12 use a hash table?

--- day12.lisp
+++ day12.lisp
@@ -4,6 +4,9 @@
   (ql:quickload '(:alexandria :str))
   (add-package-local-nickname 'a 'alexandria-2))
 
+;; (eval-when (:compile-toplevel :load-toplevel :execute)
+;;   (use-package 'defclass-std))
+
 (defparameter *day-number* 12)
 (defparameter *input-name-template* "2024d~dinput.txt")
 
@@ -38,21 +41,29 @@ MMMISSJEEE"
 (defun parse-input (lines)
   (when (typep lines 'string)
     (setf lines (str:split-omit-nulls #\newline lines)))
-  (make-array (list (length lines) (length (first lines)))
-              :element-type 'character
-              :initial-contents lines))
+  (values (make-array (list (length lines) (length (first lines)))
+                      :element-type 'character
+                      :initial-contents lines)))
 
-(defun p1 ()
-  ) 
+(defun p1 (garden)
+  (let ((plot-areas (make-hash-table :size (+ 10 (* 2 26))); A-Z,a-z,0-9
+                    )
+        (linear-garden (make-array (array-total-size garden) :displaced-to garden)))
+    (map nil (lambda (chr)
+               (incf (gethash chr plot-areas 0)))
+         linear-garden)
+    (let ((plot-types (a:hash-table-keys)))
+      )))
+ 
 
-(defun p2 ()
+(defun p2 (garden plot-types)
   )
 
-(defun run (parts-list data)
+(defun run (parts-list garden plot-types)
   (dolist (part (a:ensure-list parts-list))
     (ccase part
-      (1 (format t "~&Part 1: ~a" (p1 data)))
-      (2 (format t "~&Part 2: ~a" (p2 data))))))
+      (1 (format t "~&Part 1: ~a" (p1 garden)))
+      (2 (format t "~&Part 2: ~a" (p2 garden))))))
 
 (defun main (&rest parts)
   (let* ((infile-name (format nil *input-name-template* *day-number*))
@@ -63,6 +74,8 @@ MMMISSJEEE"
 (defun test-all (&rest parts)
   (princ (with-output-to-string (*standard-output*)
            (dolist (input (list *small-test-input*
-                                *med-test-input*
-                                *test-input*))
-             (run parts (parse-input (str:split-omit-nulls #\newline input)))))))
+                            *med-test-input*
+                            *test-input*))
+             (run parts (parse-input (str:split-omit-nulls #\newline input))))))
+  (terpri))
+
