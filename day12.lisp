dcf26f74d0293475e8d22455de506573ec2610de
ArgentCorvid <khokanson@gmail.com>
2025-01-14 22:08:50 -0600

    d12p1 first draft. does small test ok, too high for other 2 tests

--- day12.lisp
+++ day12.lisp
@@ -41,25 +41,48 @@ MMMISSJEEE"
 (defun parse-input (lines)
   (when (typep lines 'string)
     (setf lines (str:split-omit-nulls #\newline lines)))
-  (values (make-array (list (length lines) (length (first lines)))
-                      :element-type 'character
-                      :initial-contents lines)))
+  (make-array (list (length lines) (length (first lines)))
+              :element-type 'character
+              :initial-contents lines))
+
+(defparameter *directions* '((0 1) (1 0) (0 -1) (-1 0)))
+
+(defun same-neighbors-count (grid row col)
+  (flet ((oob? (itm)
+           (let ((maxs (array-dimensions grid)))
+             (or (some #'minusp itm)
+                 (some #'>= itm maxs)))))
+    (let* ((point (list row col))
+           (candidates (remove-if #'oob? (mapcar (lambda (d)
+                                                   (mapcar #'+ point d))
+                                                 *directions*))))
+      (count-if (a:curry #'char-equal (aref grid row col))
+                candidates
+                :key (a:curry #'apply #'aref grid)))))
 
 (defun p1 (garden)
-  (let ((plot-areas (make-hash-table :size (+ 10 (* 2 26))); A-Z,a-z,0-9
-                    )
-        (linear-garden (make-array (array-total-size garden) :displaced-to garden)))
-    (map nil (lambda (chr)
-               (incf (gethash chr plot-areas 0)))
-         linear-garden)
-    (let ((plot-types (a:hash-table-keys)))
-      )))
+  (let* ((table-size (+ 10 (* 2 26)))   ; A-Z,a-z,0-9
+         (garden-size (array-total-size garden))
+         (garden-dims (array-dimensions garden))
+         (plot-areas (make-hash-table :size table-size))
+         (plot-perimeters (make-hash-table :size table-size))
+         (linear-garden (make-array garden-size :displaced-to garden :element-type 'character)))
+    (loop for plot across linear-garden
+          for index from 0
+          for col = (mod index (second garden-dims))
+          for row = (floor (- index col) (first garden-dims))
+          for sneighbors = (same-neighbors-count garden row col)
+          do (incf (gethash plot plot-perimeters 0) (- 4 sneighbors))
+             (incf (gethash plot plot-areas 0)))
+    (loop for area being each hash-value of plot-areas
+          for perim being each hash-value of plot-perimeters
+          summing (* area perim))))
  
 
-(defun p2 (garden plot-types)
+(defun p2 (garden)
   )
 
-(defun run (parts-list garden plot-types)
+(defun run (parts-list garden)
   (dolist (part (a:ensure-list parts-list))
     (ccase part
       (1 (format t "~&Part 1: ~a" (p1 garden)))
@@ -71,11 +94,14 @@ MMMISSJEEE"
          (data (parse-input input-lines)))
     (run parts data)))
 
+(defun test-small (&rest parts)
+  (run parts (parse-input (str:split-omit-nulls #\newline *small-test-input*))))
+
 (defun test-all (&rest parts)
   (princ (with-output-to-string (*standard-output*)
-           (dolist (input (list *small-test-input*
-                            *med-test-input*
-                            *test-input*))
-             (run parts (parse-input (str:split-omit-nulls #\newline input))))))
-  (terpri))
+         (dolist (input (list *small-test-input*
+                              *med-test-input*
+                              *test-input*))
+           (run parts (parse-input (str:split-omit-nulls #\newline input))))))
+(terpri))
 
